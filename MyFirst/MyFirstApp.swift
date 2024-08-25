aad28293681a94b7bf97668c18f853203d82e436
Ryan Westhoelter <jwesth1@live.spcollege.edu>
2024-08-25 15:39:15 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Ryan Westhoelter on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
